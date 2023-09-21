8cea2ea52c9f539d7b9a99e6d661b1712321ca18
adarsh ps <adarshps859@gmail.com>
2023-09-21 13:46:21 +0530

    initial commit

---
+++ lib/calender/custom_calender.dart
@@ -0,0 +1,400 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+/// user for DateTime formatting
+import 'package:intl/intl.dart';
+
+/// `const CustomCalendar({
+///   Key? key,
+///   this.initialStartDate,
+///   this.initialEndDate,
+///   this.startEndDateChange,
+///   this.minimumDate,
+///   this.maximumDate,
+///   required this.primaryColor,
+/// })`
+class CustomCalendar extends StatefulWidget {
+  /// The minimum date that can be selected on the calendar
+  final DateTime? minimumDate;
+
+  /// The maximum date that can be selected on the calendar
+  final DateTime? maximumDate;
+
+  /// The initial start date to be shown on the calendar
+  final DateTime? initialStartDate;
+
+  /// The initial end date to be shown on the calendar
+  final DateTime? initialEndDate;
+
+  /// The primary color to be used in the calendar's color scheme
+  final Color primaryColor;
+
+  /// A function to be called when the selected date range changes
+  final Function(DateTime, DateTime)? startEndDateChange;
+
+  const CustomCalendar({
+    Key? key,
+    this.initialStartDate,
+    this.initialEndDate,
+    this.startEndDateChange,
+    this.minimumDate,
+    this.maximumDate,
+    required this.primaryColor,
+  }) : super(key: key);
+
+  @override
+  CustomCalendarState createState() => CustomCalendarState();
+}
+
+class CustomCalendarState extends State<CustomCalendar> {
+  List<DateTime> dateList = <DateTime>[];
+
+  DateTime currentMonthDate = DateTime.now();
+
+  DateTime? startDate;
+
+  DateTime? endDate;
+
+  @override
+  void initState() {
+    setListOfDate(currentMonthDate);
+    if (widget.initialStartDate != null) {
+      startDate = widget.initialStartDate;
+    }
+    if (widget.initialEndDate != null) {
+      endDate = widget.initialEndDate;
+    }
+    super.initState();
+  }
+
+  void setListOfDate(DateTime monthDate) {
+    dateList.clear();
+    final DateTime newDate = DateTime(monthDate.year, monthDate.month, 0);
+
+    int previousMothDay = 0; //week like sunday
+    if (newDate.weekday < 7) {
+      //!sunday
+      previousMothDay = newDate.weekday + 1;
+      for (int i = 1; i <= previousMothDay; i++) {
+        //gets previous month days
+        dateList.add(newDate.subtract(Duration(days: previousMothDay - i)));
+      }
+    }
+    for (int i = 0; i < (42 - previousMothDay); i++) {
+      dateList.add(newDate.add(Duration(days: i + 1)));
+    }
+
+  
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 4,
+            bottom: 4,
+          ),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 38,
+                  width: 38,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(24.0)),
+                      onTap: () {
+                        setState(() {
+                          currentMonthDate = DateTime(
+                              currentMonthDate.year, currentMonthDate.month, 0);
+                          setListOfDate(currentMonthDate);
+                        });
+                      },
+                      child: const Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    DateFormat('MMMM').format(currentMonthDate),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 38,
+                  width: 38,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(24.0)),
+                      onTap: () {
+                        setState(() {
+                          currentMonthDate = DateTime(currentMonthDate.year,
+                              currentMonthDate.month + 2, 0);
+                          setListOfDate(currentMonthDate);
+                        });
+                      },
+                      child: const Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 8, left: 8, bottom: 8),
+          child: Row(
+            children: getDaysNameUI(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 8, left: 8),
+          child: Column(
+            children: getDaysNoUI(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> getDaysNameUI() {
+    final List<Widget> listUI = <Widget>[];
+    for (int i = 0; i < 7; i++) {
+      listUI.add(
+        Expanded(
+          child: Center(
+            child: Text(
+              DateFormat('EEE').format(dateList[i]),
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: widget.primaryColor),
+            ),
+          ),
+        ),
+      );
+    }
+    return listUI;
+  }
+
+  List<Widget> getDaysNoUI() {
+    final List<Widget> noList = <Widget>[];
+    int count = 0;
+    for (int i = 0; i < dateList.length / 7; i++) {
+      final List<Widget> listUI = <Widget>[];
+      for (int i = 0; i < 7; i++) {
+        final DateTime date = dateList[count];
+        listUI.add(
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Stack(
+                children: <Widget>[
+                  Visibility(
+                    visible: !isEndDateRadius(date),
+                    child: Positioned(
+                      top: 14,
+                      left: 0,
+                      right: 0,
+                      height: 3,
+                      child: Container(
+                        height: 3,
+                        decoration: BoxDecoration(
+                          color: startDate != null && endDate != null
+                              ? getIsItStartAndEndDate(date) || getIsInRange(date)
+                                  ? const Color(0xFF1C9D67)
+                                  : Colors.transparent
+                              : Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(32.0)),
+                      onTap: () {
+                        if (currentMonthDate.month == date.month) {
+                          if (widget.minimumDate != null &&
+                              widget.maximumDate != null) {
+                            final DateTime newminimumDate = DateTime(
+                                widget.minimumDate!.year,
+                                widget.minimumDate!.month,
+                                widget.minimumDate!.day - 1);
+                            final DateTime newmaximumDate = DateTime(
+                                widget.maximumDate!.year,
+                                widget.maximumDate!.month,
+                                widget.maximumDate!.day + 1);
+                            if (date.isAfter(newminimumDate) &&
+                                date.isBefore(newmaximumDate)) {}
+                          } else if (widget.minimumDate != null) {
+                            final DateTime newminimumDate = DateTime(
+                                widget.minimumDate!.year,
+                                widget.minimumDate!.month,
+                                widget.minimumDate!.day - 1);
+                            if (date.isAfter(newminimumDate)) {}
+                          } else if (widget.maximumDate != null) {
+                            final DateTime newmaximumDate = DateTime(
+                                widget.maximumDate!.year,
+                                widget.maximumDate!.month,
+                                widget.maximumDate!.day + 1);
+                            if (date.isBefore(newmaximumDate)) {}
+                          } else {}
+                        }
+                      },
+                      child: Container(
+                        height: 32,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color:
+                              getIsItStartAndEndDate(date) || getIsInRange(date)
+                                  ? const Color(0xFFB5E1B2)
+                                  : Colors.transparent,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(32.0)),
+                          border: Border.all(
+                            color: getIsItStartAndEndDate(date) ||
+                                    getIsInRange(date)
+                                ? const Color(0xFF1C9D67)
+                                : Colors.transparent,
+                            width: 2,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${date.day}',
+                            style: const TextStyle(
+                              color: Color(0xFF192966),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 24,
+                    right: 16,
+                    left: 0,
+                    child: Container(
+                      height: 6,
+                      width: 6,
+                      decoration: BoxDecoration(
+                          color: DateTime.now().day == date.day &&
+                                  DateTime.now().month == date.month &&
+                                  DateTime.now().year == date.year
+                              ? getIsInRange(date)
+                                  ? Colors.white
+                                  : Colors.blue
+                              : Colors.transparent,
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+        count += 1;
+      }
+      noList.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: listUI,
+      ));
+    }
+    return noList;
+  }
+
+  bool getIsInRange(DateTime date) {
+    if (startDate != null && endDate != null) {
+      if (date.isAfter(startDate!) && date.isBefore(endDate!)) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  bool getIsItStartAndEndDate(DateTime date) {
+    if (startDate != null &&
+        startDate!.day == date.day &&
+        startDate!.month == date.month &&
+        startDate!.year == date.year) {
+      return true;
+    } else if (endDate != null &&
+        endDate!.day == date.day &&
+        endDate!.month == date.month &&
+        endDate!.year == date.year) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isStartDateRadius(DateTime date) {
+    if (startDate != null &&
+        startDate!.day == date.day &&
+        startDate!.month == date.month) {
+      return true;
+    } else if (date.weekday == 7) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool isEndDateRadius(DateTime date) {
+    if (endDate != null &&
+        endDate!.day == date.day &&
+        endDate!.month == date.month) {
+      return true;
+    } else if (date.weekday == 6) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:tester/calender/custom_calender.dart';
+import 'package:tester/calender/custom_date_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime? startDate;
+  DateTime? endDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 16, bottom: 16, top: 50, right: 16),
+          child: Material(
+            color: Colors.transparent,
+            child: CustomCalendar(
+              primaryColor: Colors.black,
+              minimumDate: DateTime.now().subtract(const Duration(days: 60)),
+              maximumDate: DateTime.now().add(const Duration(days: 30)),
+              initialStartDate: DateTime.now(),
+              initialEndDate: DateTime(2023, 10, 15),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
